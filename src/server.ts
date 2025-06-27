037393beff072aa9302b0d0fc10a78fe46878f2e
thalesbensi <thalesbensi@outlook.com>
2025-06-27 16:10:05 -0300

    fix: Implement PodcastController

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "node:http";
+import {findAllEpisodes} from './controllers/PodcastController'
 
-const server = http.createServer((req:IncomingMessage, res:http.ServerResponse) => {}
-)
+
+const server = http.createServer(
+    async (req:IncomingMessage, res:http.ServerResponse) => {
+        if (req.method === "GET") {
+            await findAllEpisodes(req,res);
+        }
+    }
+);
 
 server.listen(process.env.SERVER_PORT,
     () => console.log(`Server started at port ${process.env.SERVER_PORT}`));
